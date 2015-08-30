faa3111558d00228b80cfc94956428f64533d2af
martinusbe <martinusbe@gmail.com>
2015-08-30 21:28:12 +0200

    Add CameraNext to the device
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- tipsy.mk
+++ tipsy.mk
@@ -24,6 +24,10 @@ PRODUCT_MANUFACTURER := Oneplus
 
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
+# CameraNext
+PRODUCT_COPY_FILES += \
+	device/oneplus/bacon/prebuilt/CameraNextModV22.apk:system/app/CameraNextModV22.apk
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=oneplus/bacon/A0001:5.0.2/LRX22G/YNG1TAS2I3:user/release-keys \
     PRIVATE_BUILD_DESC="bacon-user 5.0.2 LRX22G YNG1TAS2I3 release-keys"
